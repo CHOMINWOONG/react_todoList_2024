4613a1b0f8236722cc56d27650aeab29b90e6d4d
CHOMINWOONG <alsdnd0701@naver.com>
2024-03-13 20:37:56 +0900

    timeout

---
+++ 2024-react-exam-main/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [num, setNum] = useState(0);
+
+  const timeout = setTimeout(() => setNum(num + 1), 1000);
+
+  const pause = () => clearTimeout(timeout);
+
+  const resume = () => {
+    setTimeout(() => setNum(num + 1), 1000);
+  }
+
+  return (
+    <>
+    숫자 : {num}
+    <br/>
+    <button onClick={pause}>일시정지</button>
+    <br/>    
+    <button onClick={resume}>재게</button>
+    </>
+  );
+}
+
+export default App;
