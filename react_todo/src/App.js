12286feb617792e21479b2d3d17e4ebbe88de132
CHOMINWOONG <alsdnd0701@naver.com>
2024-04-26 21:16:50 +0900

    테일윈드, MUI, MUI 테마 색상 css 변수화

--- react_todo/src/App.js
+++ react_todo/src/App.js
@@ -1,9 +1,15 @@
+import { AppBar, Toolbar } from "@mui/material";
+
 
 function App() {
   return (
-    <div>
-      <i className="fa-brands fa-twitter"></i>안녕
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <div className="flex-1"></div>
+        <div className="font-bold">TODO</div>
+        <div className="flex-1"></div>
+      </Toolbar>
+    </AppBar>
   );
 }
 
