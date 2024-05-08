c47d8cdb5b0b73b1495e25bdc301df1f6cc7421f
CHOMINWOONG <alsdnd0701@naver.com>
2024-05-08 19:45:17 +0900

    일정이 급한 순으로 정렬

--- react_todo/src/compoents/pages/TodoListPage.js
+++ react_todo/src/compoents/pages/TodoListPage.js
@@ -32,7 +32,11 @@ export default function TodoList() {
     }
         const filteredTodos = getFilteredTodos();
     
-  
+        const sortedTodos = [...filteredTodos].sort((a, b) => {
+          if ( a.performDate == b.performDate ) return 0;
+
+          return a.performDate > b.performDate ? 1 : -1
+        })
 
     return (
       <>
@@ -128,7 +132,7 @@ export default function TodoList() {
   
         <div className='mt-4 px-4'>
           <ul>
-            {filteredTodos.map((todo, index) => (
+            {sortedTodos.map((todo, index) => (
               <TodoListItem 
                 key={todo.id} 
                 todo={todo} 
