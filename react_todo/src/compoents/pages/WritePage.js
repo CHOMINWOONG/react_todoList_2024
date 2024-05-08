fc60da69c803ef427f40da48dc3db0ec5e5b9568
CHOMINWOONG <alsdnd0701@naver.com>
2024-05-08 19:32:29 +0900

    수행날짜 추가

--- react_todo/src/compoents/pages/WritePage.js
+++ react_todo/src/compoents/pages/WritePage.js
@@ -12,13 +12,13 @@ export default function WritePage() {
 
     form.content.value = form.content.value.trim();
 
-    if ( form.regDate.value.length == 0) {
+    if ( form.performDate.value.length == 0) {
       alert("날짜를 입력해주세요.");
-      form.regDate.focus();
+      form.performDate.focus();
       return;
     }
 
-    const newTodoId = todosState.addTodo(form.regDate.value, form.content.value);
+    const newTodoId = todosState.addTodo(form.performDate.value, form.content.value);
 
     noticeSnackbarState.open(`${newTodoId}번 할 일이 추가되었습니다.`);
 
@@ -34,7 +34,7 @@ export default function WritePage() {
           label="날짜 입력" 
           focused 
           type="datetime-local"
-          name="regDate"
+          name="performDate"
           />
 
           <TextField 
